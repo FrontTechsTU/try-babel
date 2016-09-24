78a350a860d4dfa7c9e48ad281637e266e2cdb06
Tanawat Tassana <amp.tanawat@gmail.com>
2016-09-24 09:21:31 +0700

    Log response

--- src/hello.js
+++ src/hello.js
@@ -7,3 +7,6 @@ console.log(sayHi(name))
 import axios from 'axios'
 const batmanUrl = 'http://www.omdbapi.com/?t=Batman&y=&plot=short&r=json'
 axios.get(batmanUrl)
+    .then(response => {
+        console.log(response)
+    })
